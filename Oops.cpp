dd7d8860376df244d48e1287bd627a51ef3fef9c
Dhruv Sharma <sdhruv.mdnr@gmail.com>
2026-09-01 16:12:40 +0530

    2nd Comp

--- Oops.cpp
+++ Oops.cpp
@@ -1,21 +1,52 @@
+// #include <iostream>
+// using namespace std;
+// class student{
+// public:
+// string name;
+// int rollno;
+// void display(){
+//     cout<<name<<endl;
+//     cout<<rollno<<endl;
+// }
+// };
+
+// int main(){
+//     student s;
+//     cout<<"Enter your name:";
+//     cin>>s.name;
+    
+//     cout<<"Enter rollno :";
+//     cin>>s.rollno;
+//     s.display();
+// }
+
+
 #include <iostream>
 using namespace std;
-class student{
-public:
+class employee{
+private:
 string name;
-int rollno;
-void display(){
-    cout<<name<<endl;
-    cout<<rollno<<endl;
-}
+int salary;
+int EmployeeId;
+public:
+void input(string name,int salary,int EmployeeId );
+void display();
 };
 
+void employee::input(string name,int salary,int EmployeeId ){
+    this->name=name;
+    this->salary=salary;
+    this->EmployeeId=EmployeeId;
+}
+
+void employee::display(){
+    cout<<"Name:"<<name<<endl;
+    cout<<"Salary:"<<salary<<endl;
+    cout<<"EmployeeId:"<<EmployeeId<<endl;
+}
+
 int main(){
-    student s;
-    cout<<"Enter your name:";
-    cin>>s.name;
-    
-    cout<<"Enter rollno :";
-    cin>>s.rollno;
-    s.display();
+    employee e;
+    e.input("Dhruv",25000,68);
+    e.display();
 }
