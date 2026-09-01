d32e37a974396709973375ba5cf0e0cf827ad3f8
Dhruv Sharma <sdhruv.mdnr@gmail.com>
2026-09-01 16:18:31 +0530

    3rd Comp

--- Oops.cpp
+++ Oops.cpp
@@ -21,32 +21,69 @@
 // }
 
 
+// #include <iostream>
+// using namespace std;
+// class employee{
+// private:
+// string name;
+// int salary;
+// int EmployeeId;
+// public:
+// void input(string name,int salary,int EmployeeId );
+// void display();
+// };
+
+// void employee::input(string name,int salary,int EmployeeId ){
+//     this->name=name;
+//     this->salary=salary;
+//     this->EmployeeId=EmployeeId;
+// }
+
+// void employee::display(){
+//     cout<<"Name:"<<name<<endl;
+//     cout<<"Salary:"<<salary<<endl;
+//     cout<<"EmployeeId:"<<EmployeeId<<endl;
+// }
+
+// int main(){
+//     employee e;
+//     e.input("Dhruv",25000,68);
+//     e.display();
+// }
+
+
+//Ques-3
 #include <iostream>
 using namespace std;
-class employee{
-private:
-string name;
-int salary;
-int EmployeeId;
-public:
-void input(string name,int salary,int EmployeeId );
-void display();
-};
-
-void employee::input(string name,int salary,int EmployeeId ){
-    this->name=name;
-    this->salary=salary;
-    this->EmployeeId=EmployeeId;
+void callByV(int a,int b){
+    int c=a;
+    a=b;
+    b=c;
+    cout<<"After swapping:"<<a<<" "<<b<<endl;
 }
 
-void employee::display(){
-    cout<<"Name:"<<name<<endl;
-    cout<<"Salary:"<<salary<<endl;
-    cout<<"EmployeeId:"<<EmployeeId<<endl;
+void callByR(int& a,int& b){
+    int c=a;
+    a=b;
+    b=c;
+    cout<<"After swapping:"<<a<<" "<<b<<endl;
 }
 
 int main(){
-    employee e;
-    e.input("Dhruv",25000,68);
-    e.display();
+    int a;
+    cout<<"Enter first No:";
+    cin>>a;
+    int b;
+    cout<<"Enter Second No:";
+cin>>b;
+cout<<"Call By Value"<<endl;
+cout<<"Before Swapping:"<<a<<" "<<b<<endl;
+callByV(a,b);
+cout<<a<<" "<<b<<endl;
+
+cout<<"Call By Reference"<<endl;
+cout<<"Before Swapping:"<<a<<" "<<b<<endl;
+callByR(a,b);
+cout<<a<<" "<<b<<endl;
+
 }
