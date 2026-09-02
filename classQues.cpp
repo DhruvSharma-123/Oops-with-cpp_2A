3ea84d8d887d85547524e96e678263111997559e
Dhruv Sharma <sdhruv.mdnr@gmail.com>
2026-09-02 09:34:41 +0530

    Static Keyword

--- classQues.cpp
+++ classQues.cpp
@@ -1,19 +1,35 @@
-//Destructors    
+                                                            //Destructors    
+// #include <iostream>
+// using namespace std;
+// class obj{
+// public:
+// string name;
+// obj(){
+//     cout<<"I am constructre"<<endl;
+// }
+// ~obj(){
+//     cout<<"I am Destructer";
+// }
+// };
+
+// int main(){
+//     obj a;
+//     a.name="Dhruv";
+//     cout<<a.name<<endl;
+// }
+
+     //                                               Static KeyWord
 #include <iostream>
 using namespace std;
-class obj{
-public:
-string name;
-obj(){
-    cout<<"I am constructre"<<endl;
-}
-~obj(){
-    cout<<"I am Destructer";
-}
-};
+class sta{
+    public:
+    static int count;
 
+};  
+
+int sta::count=56;
 int main(){
-    obj a;
-    a.name="Dhruv";
-    cout<<a.name<<endl;
+    stat s;
+
+    cout<<sta::count<<endl;
 }
