7f926fb1ee333131c935134cc480c7450cc57bf4
Dhruv Sharma <sdhruv.mdnr@gmail.com>
2026-09-03 11:28:10 +0530

    Copy Constructer

--- classQues.cpp
+++ classQues.cpp
@@ -19,17 +19,82 @@
 // }
 
      //                                               Static KeyWord
+// #include <iostream>
+// using namespace std;
+// class sta{
+//     public:
+//     static int count;
+
+// };  
+
+// int sta::count=56;
+// int main(){
+//     stat s;
+
+//     cout<<sta::count<<endl;
+// }
+
+
+                                     //  Copy constructer
+// #include <iostream>
+// using namespace std;                                     
+// class student{
+//     public:
+//     string name;
+//     int marks;
+//     student(string name,int marks){
+// this->name=name;
+// this->marks=marks;
+//     }
+//     student(const student& s){
+// name=s.name;
+// marks=s.marks;
+//     }
+
+//     void display(){
+// cout<<name<<endl;
+// cout<<marks<<endl;
+//     }
+// };
+
+// int main(){
+//     student s1("Dhruv",100);
+// student s2(s1);
+// s2.display();
+// }
+
+
 #include <iostream>
-using namespace std;
-class sta{
+using namespace std;                                     
+class student{
+     double* cgpa;
     public:
-    static int count;
+    string name;
+    int marks;
 
-};  
+    student(string name, int marks, double cgpa) {
+    this->name = name;
+    this->marks = marks;
 
-int sta::count=56;
+    this->cgpa = new double;
+    *(this->cgpa) = cgpa;
+}
+
+
+    student(const student& s){
+name=s.name;
+marks=s.marks;
+this->cgpa=s.cgpa;
+    }
+
+    void display(){
+cout<<name<<endl;
+cout<<marks<<endl;
+    }
+};
+
 int main(){
-    stat s;
-
-    cout<<sta::count<<endl;
+    student s1("Dhruv",90 ,9.5);
+student s2(s1);
+s2.display();
 }
